e946e4c8dab0887897344c7d2dedb0fbe10e1512
Tamara Takács-Szabados <tszabados9@gmail.com>
2026-06-04 17:14:17 +0200

    impressum added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 import { CookiePolicy } from './pages/cookie-policy/cookie-policy';
+import { Impressum } from './pages/impressum/impressum';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'cookie-tajekoztato',
     component: CookiePolicy,
+  },
+  {
+    path: 'impresszum',
+    component: Impressum,
   },
   { path: '', component: Home },
   { path: '**', redirectTo: '' },
---
+++ src/app/pages/impressum/impressum.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Impressum } from './impressum';
+
+describe('Impressum', () => {
+  let component: Impressum;
+  let fixture: ComponentFixture<Impressum>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Impressum],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Impressum);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
